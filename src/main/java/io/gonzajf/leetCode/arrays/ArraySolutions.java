b084c1e6e9efa40f567aa5c8de6391411ccbff89
Gonzalo Franchino <gonzalo.franchino@gmail.com>
2020-09-04 18:09:41 -0300

    replace elements solution

--- src/main/java/io/gonzajf/leetCode/arrays/ArraySolutions.java
+++ src/main/java/io/gonzajf/leetCode/arrays/ArraySolutions.java
@@ -172,4 +172,24 @@ public class ArraySolutions {
     	}
     	return true;
     }
+    
+    /**
+     * Given an array arr, replace every element in that array with the greatest 
+     * element among the elements to its right, and replace the last element with -1.
+     * After doing so, return the array.
+     */
+    public static int[] replaceElements(int[] arr) {
+        
+    	int max = arr[arr.length-1];
+    	arr[arr.length-1] = -1;
+    	
+    	for (int i = arr.length-1; i > 0 ; i--) {
+    		int prev = arr[i-1];
+    		arr[i-1] = max;
+    		if(prev > max) {
+    			max = prev;
+    		}
+		}
+    	return arr;
+    }
 }
