6186e37ab14d36eb49910c3c9683eec0340dbeb4
Gonzalo Franchino <gonzalo.franchino@gmail.com>
2020-09-11 15:41:41 -0300

    solution to intersection of linked lists problem

--- src/main/java/io/gonzajf/leetCode/linkedList/ListNode.java
+++ src/main/java/io/gonzajf/leetCode/linkedList/ListNode.java
@@ -69,4 +69,36 @@ public class ListNode {
 		}
 		return slow;
 	}
+	
+	/**
+	 * Write a program to find the node at which the intersection of two singly linked lists begins.
+	 */
+    public static ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        
+    	if(headA == null || headB == null) {
+    		return null;
+    	}
+    	
+    	ListNode p1 = headA;
+    	ListNode p2 = headB;
+   
+    	while(p1 != p2) {
+    		
+    		p1 = p1.next;
+    		p2 = p2.next;
+    		
+    		if(p1 == p2) {
+    			return p1;
+    		}
+    		
+    		if(p1 == null) {
+    			p1 = headB;
+    		}
+    		
+    		if(p2 == null) {
+    			p2 = headA;
+    		}
+    	}
+    	return p1;
+    }
 }
