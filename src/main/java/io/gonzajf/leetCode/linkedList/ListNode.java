54c9a9da2aa93cddd43a162b089b301511152186
Gonzalo Franchino <gonzalo.franchino@gmail.com>
2020-09-13 21:49:19 -0300

    solution to delete nth from the end node problem

--- src/main/java/io/gonzajf/leetCode/linkedList/ListNode.java
+++ src/main/java/io/gonzajf/leetCode/linkedList/ListNode.java
@@ -35,11 +35,11 @@ public class ListNode {
 	}
 
 	/**
-	 * Given a linked list, return the node where the cycle begins. 
-	 * If there is no cycle, return null.
-	 * To represent a cycle in the given linked list, we use an integer pos which represents 
-	 * the position (0-indexed) in the linked list where tail connects to. 
-	 * If pos is -1, then there is no cycle in the linked list.
+	 * Given a linked list, return the node where the cycle begins. If there is no
+	 * cycle, return null. To represent a cycle in the given linked list, we use an
+	 * integer pos which represents the position (0-indexed) in the linked list
+	 * where tail connects to. If pos is -1, then there is no cycle in the linked
+	 * list.
 	 */
 	public static ListNode detectCycle(ListNode head) {
 
@@ -69,36 +69,60 @@ public class ListNode {
 		}
 		return slow;
 	}
-	
+
 	/**
-	 * Write a program to find the node at which the intersection of two singly linked lists begins.
+	 * Write a program to find the node at which the intersection of two singly
+	 * linked lists begins.
 	 */
-    public static ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-        
-    	if(headA == null || headB == null) {
-    		return null;
-    	}
-    	
-    	ListNode p1 = headA;
-    	ListNode p2 = headB;
-   
-    	while(p1 != p2) {
-    		
-    		p1 = p1.next;
-    		p2 = p2.next;
-    		
-    		if(p1 == p2) {
-    			return p1;
-    		}
-    		
-    		if(p1 == null) {
-    			p1 = headB;
-    		}
-    		
-    		if(p2 == null) {
-    			p2 = headA;
-    		}
-    	}
-    	return p1;
-    }
+	public static ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+
+		if (headA == null || headB == null) {
+			return null;
+		}
+
+		ListNode p1 = headA;
+		ListNode p2 = headB;
+
+		while (p1 != p2) {
+
+			p1 = p1.next;
+			p2 = p2.next;
+
+			if (p1 == p2) {
+				return p1;
+			}
+
+			if (p1 == null) {
+				p1 = headB;
+			}
+
+			if (p2 == null) {
+				p2 = headA;
+			}
+		}
+		return p1;
+	}
+
+	/**
+	 * Given a linked list, remove the n-th node from the end of list and return its head.
+	 */
+	public static ListNode removeNthFromEnd(ListNode head, int n) {
+
+		ListNode dummy = new ListNode(0);
+		dummy.next = head;
+		ListNode first = dummy;
+		ListNode second = dummy;
+		// Advances first pointer so that the gap between first and second is n nodes
+		// apart
+		for (int i = 1; i <= n + 1; i++) {
+			first = first.next;
+		}
+		// Move first to the end, maintaining the gap
+		while (first != null) {
+			first = first.next;
+			second = second.next;
+		}
+		second.next = second.next.next;
+		return dummy.next;
+	}
 }
