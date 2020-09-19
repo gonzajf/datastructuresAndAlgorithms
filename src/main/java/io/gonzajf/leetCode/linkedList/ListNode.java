f030571c2b4ca3da014e9eed6a4105c2721ece1e
Gonzalo Franchino <gonzalo.franchino@gmail.com>
2020-09-19 19:30:52 -0300

    merge two sorted lists

--- src/main/java/io/gonzajf/leetCode/linkedList/ListNode.java
+++ src/main/java/io/gonzajf/leetCode/linkedList/ListNode.java
@@ -231,4 +231,33 @@ public class ListNode {
 
 		return prevNode;
 	}
+
+	/**
+	 * Merge two sorted linked lists and return it as a new sorted list. The new
+	 * list should be made by splicing together the nodes of the first two lists.
+	 */
+	public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+
+		ListNode dummy = new ListNode(-1);
+		ListNode head = dummy;
+
+		while (l1 != null && l2 != null) {
+
+			if (l1.val <= l2.val) {
+				dummy.next = l1;
+				l1 = l1.next;
+			} else {
+				dummy.next = l2;
+				l2 = l2.next;
+			}
+			dummy = dummy.next;
+		}
+
+		if (l1 != null) {
+			dummy.next = l1;
+		} else {
+			dummy.next = l2;
+		}
+		return head.next;
+	}
 }
